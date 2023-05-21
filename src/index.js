3c9d75a93752c02afafb4589893c874f8b8a179d
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-05-21 17:01:18 +0530

    complete cli implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-import { cloneWebApp } from './cloner.js'
+import { cloneWebApp } from './cloner.js';
+import { getWebAppMapUrl, getOutputDirPath} from './utils.js'
+
+
 // Google Dork: ext:map intext:react inurl:app.js.map
-const srcMapUrl = "https://iam.innogy.com/js/innogy/app.js.map";
-// const outputDirPath = path.join(process.cwd(), 'temp');
-const outputDirPath = 'D:\\GithubRepos\\web-app-cloner\\temp';
+// const srcMapUrl = "https://iam.innogy.com/js/innogy/app.js.map";
+const srcMapUrl = await getWebAppMapUrl();
+const outputDirPath = await getOutputDirPath();
 
 cloneWebApp(srcMapUrl, outputDirPath);
